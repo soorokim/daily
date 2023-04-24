3eece6a3403a86e1691a4c297c9504d750f4cb5a
soorokim <rlatnfh99@gmail.com>
2023-04-24 18:46:02 +0900

    230424 백준 문제풀이

---
+++ baeckjoon/retry/2447/2447.q.js
@@ -0,0 +1,37 @@
+const fs = require("fs");
+
+const inputs = fs
+  // .readFileSync("/dev/stdin")
+  .readFileSync(__dirname + "/test.txt")
+  .toString()
+  .trim()
+  .split("\n")
+  .map(Number);
+
+let result = "";
+
+let c = 0;
+const recursive = (i, j, n) => {
+  if (Math.floor(i / n) % 3 === 1 && Math.floor(j / n) % 3 === 1) {
+    result += " ";
+  } else {
+    if (n / 3 === 0) {
+      result += "*";
+    } else {
+      recursive(i, j, Math.floor(n / 3));
+    }
+  }
+};
+const draw = (n) => {
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      recursive(i, j, n);
+    }
+    result += "\n";
+  }
+};
+draw(27);
+console.log(result);
+
+// console.log(getBase3Log(27));
+// console.log("***************************".length / 3 / 3 / 3);
